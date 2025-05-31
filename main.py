f9532aedb3ce87de33a0d4dac86c6791fa18b55d
Caramajau <caramia0505@gmail.com>
2025-05-31 18:48:09 +0200

    Use actual links

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from typing import Mapping
+from model.code_website_handler import CodeWebsiteHandler
 from model.url_opener import URLOpener
 
 
 def main() -> None:
     url_opener: URLOpener = URLOpener()
-    test_url: str = "https://vecka.nu/"
-    url_opener.set_urls([test_url])
-    url_opener.open_urls()
+
+    website_handler: CodeWebsiteHandler = CodeWebsiteHandler()
+    websites: Mapping[str, list[str]] = website_handler.get_websites()
+    print("The following games have links:")
+    for game in websites.keys():
+        print(game)
+    print()
+    selected_game: str = input("Choose a game: ")
+
+    if selected_game in websites.keys():
+        url_opener.set_urls(websites[selected_game])
+        url_opener.open_urls()
+    else:
+        main()
 
 
 if __name__ == "__main__":
---
+++ model/code_website_handler.py
@@ -0,0 +1,31 @@
+import os
+from typing import Final, Mapping
+
+from model.json_handler import JSONHandler
+
+
+class CodeWebsiteHandler:
+    # Only "recent" ones included and also just game8 as they seemed to have a quite good list.
+    DEFAULT_WEBSITES: Final[Mapping[str, list[str]]] = {
+        "sv": ["https://game8.co/games/Pokemon-Scarlet-Violet/archives/384443"],
+        "la": ["https://game8.co/games/Pokemon-Legends-Arceus/archives/353314"],
+        "bdsp": [
+            "https://game8.co/games/Pokemon-Brilliant-Diamond-Shining-Pearl/archives/342366"
+        ],
+        "swsh": ["https://game8.co/games/pokemon-sword-shield/archives/280987"],
+    }
+
+    DATA_PATH: Final[str] = os.path.join("data", "websites.json")
+
+    def __init__(self) -> None:
+        json_handler: JSONHandler = JSONHandler(CodeWebsiteHandler.DATA_PATH)
+
+        websites: Mapping[str, list[str]] = json_handler.read_json()
+        if len(websites) < 1:
+            self.__websites: Mapping[str, list[str]] = (
+                CodeWebsiteHandler.DEFAULT_WEBSITES
+            )
+            json_handler.write_json(self.__websites)
+
+    def get_websites(self) -> Mapping[str, list[str]]:
+        return self.__websites
