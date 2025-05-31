83bf1ff09a475856706fe28bcfcb426e0d8fd349
Caramajau <caramia0505@gmail.com>
2025-05-31 17:51:05 +0200

    Create URLOpener

---
+++ model/url_opener.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+class URLOpener():
+    def __init__(self) -> None:
+        self.__urls: list[str] = []
+
+    def set_urls(self, new_urls: list[str]) -> None:
+        self.__urls = new_urls
+
+    def open_urls(self) -> None:
+        for url in self.__urls:
+            self.__open_url(url)
+
+    @staticmethod
+    def __open_url(url: str) -> None:
+        webbrowser.open(url)
