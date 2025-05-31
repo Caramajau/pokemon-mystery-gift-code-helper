bc7fec374c9e70be61bcb3fb01d6d37550e2d1c9
Caramajau <caramia0505@gmail.com>
2025-05-31 19:18:43 +0200

    Create build script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="pokemon-mystery-gift-code-helper",
+    version="1.0",
+    description="A helper tool for Pokémon Mystery Gift codes",
+    executables=[Executable("main.py", base=None, target_name="pokemon-mystery-gift-code-helper")]
+)
