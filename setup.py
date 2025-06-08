113b06a196508c5c023786d1a9b6b367627ffdf9
Caramajau <caramia0505@gmail.com>
2025-06-08 09:45:58 +0200

    Format setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from cx_Freeze import setup, Executable
+from cx_Freeze import setup, Executable  # type: ignore
 
 setup(
     name="pokemon-mystery-gift-code-helper",
     version="1.0",
     description="A helper tool for Pokémon Mystery Gift codes",
-    executables=[Executable("main.py", base=None, target_name="PokemonMysteryGiftCodeHelper")]
+    executables=[
+        Executable("main.py", base=None, target_name="PokemonMysteryGiftCodeHelper")
+    ],
 )
