735821ce41903f73fb32875b6365e5f97d3c0707
Caramajau <caramia0505@gmail.com>
2025-07-08 16:51:53 +0200

    Format url opener

--- model/url_opener.py
+++ model/url_opener.py
@@ -1,9 +1,11 @@
 from collections.abc import Iterable
 import webbrowser
 
+
 def open_urls(urls: Iterable[str]) -> None:
     for url in urls:
-       open_url(url)
+        open_url(url)
+
 
 def open_url(url: str) -> None:
     webbrowser.open(url)
