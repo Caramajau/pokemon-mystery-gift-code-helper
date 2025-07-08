abe9b9d3869e78369c57b4a71b15c9b3f37bedc5
Caramajau <caramia0505@gmail.com>
2025-07-08 15:59:12 +0200

    Simplify url opener code

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from typing import Mapping
 from model.code_website_handler import CodeWebsiteHandler
-from model.url_opener import URLOpener
+from model.url_opener import open_urls
 
 
 def main() -> None:
-    url_opener: URLOpener = URLOpener()
     website_handler: CodeWebsiteHandler = CodeWebsiteHandler()
     websites: Mapping[str, list[str]] = website_handler.get_websites()
 
-    process_game_selection(url_opener, websites)
+    process_game_selection(websites)
 
 
-def process_game_selection(
-    url_opener: URLOpener, websites: Mapping[str, list[str]]
-) -> None:
+def process_game_selection(websites: Mapping[str, list[str]]) -> None:
     print_available_games(websites)
 
     selected_game: str = input("Choose a game: ")
 
     if selected_game in websites.keys():
-        url_opener.set_urls(websites[selected_game])
-        url_opener.open_urls()
+        open_urls(websites[selected_game])
     else:
-        process_game_selection(url_opener, websites)
+        process_game_selection(websites)
 
 
 def print_available_games(websites: Mapping[str, list[str]]) -> None:
--- model/url_opener.py
+++ model/url_opener.py
@@ -1,17 +1,9 @@
+from collections.abc import Iterable
 import webbrowser
 
+def open_urls(urls: Iterable[str]) -> None:
+    for url in urls:
+       open_url(url)
 
-class URLOpener:
-    def __init__(self) -> None:
-        self.__urls: list[str] = []
-
-    def set_urls(self, new_urls: list[str]) -> None:
-        self.__urls = new_urls
-
-    def open_urls(self) -> None:
-        for url in self.__urls:
-            self.__open_url(url)
-
-    @staticmethod
-    def __open_url(url: str) -> None:
-        webbrowser.open(url)
+def open_url(url: str) -> None:
+    webbrowser.open(url)
