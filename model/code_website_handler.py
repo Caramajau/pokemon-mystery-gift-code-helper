ca8254e7bd0a9976c4ac43c9039fa20241882bf0
Caramajau <caramia0505@gmail.com>
2025-08-12 13:41:00 +0200

    Add spin-off game links

--- model/code_website_handler.py
+++ model/code_website_handler.py
@@ -6,14 +6,21 @@ from json_handler_caramajau.json_handler import JSONHandler
 
 
 class CodeWebsiteHandler:
-    # Only "recent" ones included and also just game8 as they seemed to have good lists.
+    # Only "recent" ones included and mostly game8 as they seemed to have good lists.
     DEFAULT_WEBSITES: Final[Mapping[str, list[str]]] = {
+        # Mainline games
         "sv": ["https://game8.co/games/Pokemon-Scarlet-Violet/archives/384443"],
         "la": ["https://game8.co/games/Pokemon-Legends-Arceus/archives/353314"],
         "bdsp": [
             "https://game8.co/games/Pokemon-Brilliant-Diamond-Shining-Pearl/archives/342366"
         ],
         "swsh": ["https://game8.co/games/pokemon-sword-shield/archives/280987"],
+        # Spin-off games
+        "unite": ["https://game8.co/games/Pokemon-UNITE/archives/355379"],
+        "go": ["https://www.pockettactics.com/pokemon-go/codes"],
+        "ptcgp": [
+            "https://www.ign.com/wikis/pokemon-tcg-pocket/Pokemon_TCG_Pocket_Gift_Codes"
+        ],
     }
 
     DATA_PATH: Final[str] = path.join("data", "websites.json")
