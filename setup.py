ab04d1df5ab92e305220bff1d07df1fe36ad245a
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-10-24 16:51:38 +0200

    Change version to 0.1.1 in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import Executable, setup  # type: ignore
 
 setup(
     name="pokemon-mystery-gift-code-helper",
-    version="0.1.0",
+    version="0.1.1",
     description="A helper tool for Pokémon Mystery Gift codes",
     executables=[
         Executable("main.py", base=None, target_name="PokemonMysteryGiftCodeHelper")
